58223659f78ee984a9bc63a0c3b1bb3a6c2ba0d4
Hewitt02 <Hewitt1203@gmail.com>
2023-11-01 06:42:46 -0400

    Will

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,43 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Display the current date in the header
+  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+
+  // Function to apply past, present, or future class to time blocks
+  function updateBlockColors() {
+    var currentHour = dayjs().hour();
+
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+
+  updateBlockColors();
+
+  // Function to load saved events from local storage
+  function loadEvents() {
+    for (var i = 9; i <= 17; i++) {
+      var eventText = localStorage.getItem("event-" + i);
+      if (eventText !== null) {
+        $("#hour-" + i + " .description").val(eventText);
+      }
+    }
+  }
+
+  loadEvents();
+
+  // Click event listener for save buttons
+  $(".saveBtn").on("click", function () {
+    var hour = $(this).closest(".time-block").attr("id").split("-")[1];
+    var eventText = $(this).siblings(".description").val();
+    localStorage.setItem("event-" + hour, eventText);
+  });
 });
+
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # WorkDayScheduler
+## Description
+
+This is an app that is designed to give the user a schedule planner for their everyday Work Day. You can add events to different times of day of your workday.
+
+
+## Table of Contents 
+
+/
+
+## Installation
+
+/
+
+## Usage
+
+This web application can be used to make a clear plan for your normal work day or just for task throughout the day.
+
+## Credits
+
+© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
